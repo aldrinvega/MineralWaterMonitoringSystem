c8e89f6e9b5662adf127409fe340369b6bef5807
aldrin0408av <aldrinmundovega@gmail.com>
2023-06-09 10:29:48 +0800

    Add "AddCash"

---
+++ Features/Payers/AddCash.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using MineralWaterMonitoring.Data;
+
+namespace MineralWaterMonitoring.Features.Payers.Exceptions;
+
+public class AddCash
+{
+    public class AddCashCommand : IRequest<Unit>
+    {
+        public Guid Id { get; set; }
+        public int Amount { get; set; }
+    }
+
+    public class Handler : IRequestHandler<AddCashCommand, Unit>
+    {
+        private readonly DataContext _context;
+
+        public Handler(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Unit> Handle(AddCashCommand command, CancellationToken cancellationToken)
+        {
+            var user = await _context.Payers.FirstOrDefaultAsync(x => x.Id == command.Id, cancellationToken);
+
+            if (user == null)
+                throw new NoPayersFoundException();
+
+            user.Balance += command.Amount;
+            await _context.SaveChangesAsync(cancellationToken);
+            return Unit.Value;
+        }
+    }
+}
